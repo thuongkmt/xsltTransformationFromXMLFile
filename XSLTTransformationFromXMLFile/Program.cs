1ba16e3ec08dc702b3bf8ab70194e5c8bc265fe9
Thuong Nguyen <m-thuongnn@tihg.onmicrosoft.com>
2023-03-01 16:57:56 +0700

    add the new xslt

--- XSLTTransformationFromXMLFile/Program.cs
+++ XSLTTransformationFromXMLFile/Program.cs
@@ -13,16 +13,25 @@ namespace XSLTTransformationFromXMLFile
         private const string sourceFile = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\Allocation_output.xml";
         private const string stylesheet = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\Allocation2DallasSO.xslt";
         private const string outputFile = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\output.xml";
+
+        private const string sourceFile2 = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\output_without_group.xml";
+        private const string stylesheet2 = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\Allocation2DallasSOGroup.xslt";
+        private const string outputFile2 = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\output_with_group.xml";
         static void Main(string[] args)
         {
             // Enable XSLT debugging.
             XslCompiledTransform xslt = new XslCompiledTransform(true);
             // Compile the style sheet.
             xslt.Load(stylesheet, new XsltSettings { EnableScript = true }, null);
-
             // Execute the XSLT transform.
             FileStream outputStream = new FileStream(outputFile, FileMode.Append);
             xslt.Transform(sourceFile, null, outputStream);
+
+            // Compile the style sheet.
+            xslt.Load(stylesheet2, new XsltSettings { EnableScript = true }, null);
+            // Execute the XSLT transform.
+            FileStream outputStream2 = new FileStream(outputFile2, FileMode.Append);
+            xslt.Transform(sourceFile2, null, outputStream);
         }
     }
 }
