fb968f497ebb4f625dd4763a3fd8be8eed2d66e0
Thuong Nguyen <m-thuongnn@tihg.onmicrosoft.com>
2023-03-02 16:29:24 +0700

    add new allocation

---
+++ XSLTTransformationFromXMLFile/Allocation/RawAllocationToAllocationGroup.xslt
@@ -0,0 +1,43 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+
+  <xsl:output method="xml" indent="yes"/>
+  <xsl:key name="groups" match="/Root/Item" use="SONo" />
+
+  <xsl:template match="/Root">
+    <Root>
+      <xsl:apply-templates select="Item[generate-id() = generate-id(key('groups', SONo)[1])]"/>
+    </Root>
+  </xsl:template>
+  <xsl:template match="Item">
+    <Item>
+      <Header>
+        <StoreNo>
+          <xsl:value-of select="StoreNo"/>
+        </StoreNo>
+        <ETA_to_Store>
+          <xsl:value-of select="ETA_to_Store"/>
+        </ETA_to_Store>
+        <SONo>
+          <xsl:value-of select="SONo"/>
+        </SONo>
+        <StoreRef>
+          <xsl:value-of select="StoreRef"/>
+        </StoreRef>
+      </Header>
+
+      <xsl:for-each select="key('groups', SONo)">
+        <Detail>
+          <SKU>
+            <xsl:value-of select="SKU"/>
+          </SKU>
+          <Qty>
+            <xsl:value-of select="Qty"/>
+          </Qty>
+        </Detail>
+      </xsl:for-each>
+    </Item>
+  </xsl:template>
+
+</xsl:stylesheet>
--- XSLTTransformationFromXMLFile/Program.cs
+++ XSLTTransformationFromXMLFile/Program.cs
@@ -10,30 +10,19 @@ namespace XSLTTransformationFromXMLFile
 {
     internal class Program
     {
-        private const string sourceFile = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\Allocation_output.xml";
-        private const string stylesheet = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\Allocation2DallasSO.xslt";
-        private const string outputFile = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\output.xml";
-
-        private const string sourceFile2 = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\output_without_group.xml";
-        private const string stylesheet2 = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\Allocation2DallasSOGroup.xslt";
-        private const string outputFile2 = @"D:\MyProject\DotNet\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\output_group.xml";
+        private const string rootPath = @"D:\\MyProject\\DotNet\\XSLTValidatorByXML\\XSLTTransformationFromXMLFile\\Allocation";
+        private static string sourceFile = $@"{rootPath}\\RawAllocation.xml";
+        private static string stylesheet = $@"{rootPath}\\RawAllocationToAllocationGroup.xslt";
+        private static string outputFile = $@"{rootPath}\\output.xml";
         static void Main(string[] args)
         {
-            /*// Enable XSLT debugging.
+            // Enable XSLT debugging.
             XslCompiledTransform xslt = new XslCompiledTransform(true);
             // Compile the style sheet.
             xslt.Load(stylesheet, new XsltSettings { EnableScript = true }, null);
             // Execute the XSLT transform.
-            FileStream outputStream = new FileStream(outputFile, FileMode.Append);
-            xslt.Transform(sourceFile, null, outputStream);*/
-
-            // Enable XSLT debugging.
-            XslCompiledTransform xslt2 = new XslCompiledTransform(true);
-            // Compile the style sheet.
-            xslt2.Load(stylesheet2);
-            // Execute the XSLT transform.
-            FileStream outputStream2 = new FileStream(outputFile2, FileMode.Create);
-            xslt2.Transform(sourceFile2, null, outputStream2);
+            FileStream outputStream = new FileStream(outputFile, FileMode.Create);
+            xslt.Transform(sourceFile, null, outputStream);
 
             Console.WriteLine("---------------------------------^---------------------------------");
             Console.ReadLine();
